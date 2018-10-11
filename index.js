0338a88ef2b069ce77f37ababb347b41b630b0a4
Matthew Fine <mfine@wesleyan.edu>
2018-10-11 01:22:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,20 +11,68 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+  item = {
+    itemName: item,
+    itemPrice: Math.floor((Math.random() * 10) + 1)
+  };
+  cart.push(item);
+  return `${item.itemName} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length === 0) {
+    return `Your shopping cart is empty.`
+  } else if (cart.length === 1) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+  } else if (cart.length === 2) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+  } else {
+    for (var i = 2; i < cart.length; i++) {
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, ${cart[1].itemName} at $${cart[1].itemPrice}, and ${cart[i].itemName} at $${cart[i].itemPrice}.`
+    }
+  }
+  
 }
 
 function total() {
   // write your code here
+  var total = 0;
+  for (var i = 0; i < cart.length; i++) {
+    total += cart[i].itemPrice;
+  }
+  return total;
 }
 
 function removeFromCart(item) {
   // write your code here
+  var itemInCart = false;
+  for(var i = 0; i < cart.length; i++){
+    if (cart[i].hasOwnProperty(item) === true) {
+      itemInCart = true;
+      cart.splice(i, 1)
+    } else {
+      return `That item is not in your cart.`
+    }
+  } 
+  // var itemInCart = false
+  // for(var i = 0; i < cart.length; i++){
+  //   if (cart[i].hasOwnProperty(item)) {
+  //     itemInCart = true;
+  //     cart.splice(i, 1);
+  //   }
+  // }
+  // if (!itemInCart) {
+  //   return "That item is not in your cart.";
+  // }
 }
-
-function placeOrder(cardNumber) {
-  // write your code here
+ function placeOrder(cardNumber) {
+ // write your code here
+   if (cardNumber === undefined) {
+     return `Sorry, we don't have a credit card on file for you.`
+   } else {
+    var sum = total();
+    cart = [];
+    return `Your total cost is $${sum}, which will be charged to the card ${cardNumber}.`;
+   }
 }
